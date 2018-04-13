9a64110ebc6623eefed2154290ac756a18857b1e
zf <fu.zhao@xtimeboys.com>
2018-04-13 17:48:44 +0800

    SpringBoot学习训练一

--- SpringBoot1/src/main/java/com/zf/springboot/Application.java
+++ SpringBoot1/src/main/java/com/zf/springboot/Application.java
@@ -1,5 +1,6 @@
 package com.zf.springboot;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- SpringBoot1/src/test/java/com/zf/springboot/SpringDemo1ApplicationTests.java
+++ SpringBoot1/src/test/java/com/zf/springboot/SpringDemo1ApplicationTests.java
@@ -1,21 +1,6 @@
 package com.zf.springboot;
 
 import com.zf.springboot.controller.HelloController;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockServletContext;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import static org.hamcrest.Matchers.equalTo;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = MockServletContext.class)
