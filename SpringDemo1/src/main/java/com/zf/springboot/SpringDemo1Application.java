ecbd637882856b66aef7468c8ba6f1e8879b5a61
zf <fu.zhao@xtimeboys.com>
2018-04-13 15:57:24 +0800

    SpringBoot练习一

---
+++ SpringDemo1/src/main/java/com/zf/springboot/SpringDemo1Application.java
@@ -0,0 +1,12 @@
+package com.zf.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemo1Application.class, args);
+	}
+}
