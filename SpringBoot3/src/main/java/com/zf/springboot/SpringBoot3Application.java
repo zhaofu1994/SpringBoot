efed648e64eaefadfd75b58fb3c5792e86b71289
zf <fu.zhao@xtimeboys.com>
2018-04-13 17:00:21 +0800

    SpringBoot学习训练三

---
+++ SpringBoot3/src/main/java/com/zf/springboot/SpringBoot3Application.java
@@ -0,0 +1,12 @@
+package com.zf.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3Application.class, args);
+	}
+}
