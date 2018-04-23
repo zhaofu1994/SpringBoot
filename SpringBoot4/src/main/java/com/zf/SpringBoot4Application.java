87d0e088ec7332b1a40cd055fa6303228286f420
zf <fu.zhao@xtimeboys.com>
2018-04-23 15:27:49 +0800

    SpringBoot学习训练四

---
+++ SpringBoot4/src/main/java/com/zf/SpringBoot4Application.java
@@ -0,0 +1,18 @@
+package com.zf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : zf
+ * @描述:
+ * @创建: 2018/4/19-15:53;
+ * @版本: V1.0
+ **/
+@SpringBootApplication
+public class SpringBoot4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot4Application.class, args);
+    }
+}
